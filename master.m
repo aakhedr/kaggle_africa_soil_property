fedcc0739f301a77083c197a1cac2a8527b464cd
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-09-29 01:43:57 +0200

    Apply normal equation on train and test sets

--- master.m
+++ master.m
@@ -3,9 +3,43 @@ clear, close all; clc
 
 % read data into MATLAB
 % Topsoil=1 & Subsoil=0
-data = csvread('training.csv', 1, 1)
+fprintf('Loading training.csv data into MATLAB...\n')
+data = csvread('training.csv', 1, 1);
 
-% train data = 60% of total (~= 695 rows)
-train = data(1:695, :);
+fprintf('Dividing data into train and test sets...\n\n')
+% train data = 70% of total (~= 800 rows)
+train = data(1:800, :);
 x = train(:, 1:3594); y = train(:, 3595:end);
 [m, n] = size(x);
+
+% add intercept term to x
+X = [ones(m, 1) x];
+%=================================================================
+
+fprintf('Solving with normal equation...\n')
+% calculate the parameters from the normal equation
+theta = normalEqn(X, y);
+fprintf('Calculated the parameters using the normal equation.\n\n');
+%=================================================================
+%% Running parameters on the test set %%
+fprintf('Trying the parameters learned on the test set...\n');
+test = data(801:end, :);
+xtest = test(:, 1:3594); ytest = test(:, 3595:end);
+[l, k] = size(xtest);
+Xtest = [ones(l, 1) xtest];
+
+predictTest = Xtest * theta;
+% calculate SSE
+SSE = sum((predictTest - ytest).^2);
+
+totalErrors = sqrt(SSE);
+fprintf('Total errors per column on 357 examples (test set) are:\n');
+disp(totalErrors);
+%========================================================================
+%% Running parameters on the training set too %%
+fprintf('Total errors per column on 800 examples (training set) are:\n');
+predict = X * theta;
+errors = sqrt(sum(predict - y).^2);
+disp(errors);
+%==========================================================================
+
