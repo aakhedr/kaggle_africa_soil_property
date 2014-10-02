70b1a582fb4fa660dd2d67d3fa42e9ebdf96f775
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-02 21:44:19 +0200

    Complete learning curve and validation curve

--- linearRegression.m
+++ linearRegression.m
@@ -3,35 +3,35 @@ clear; close all, clc;
 data = csvread('training.csv', 1, 1);
 
 % 60% of 1157 observations ~= 695 examples (training set)
-X = data(1:695, 1:3594); y = data(1:695, 3595);
+X = data(1:695, 1:3594); y = data(1:695, 3595);		% Ca label only
 
 [m, n] = size(X);
 % add intercept term
 X = [ones(m, 1) X];
 %==============================================================================
 % Cross validation set (20% of 1157 observations ~= 231 examples)
-Xval = data(696:925, 1:3594); yval = data(696:925, 3595);
+Xval = data(696:925, 1:3594); yval = data(696:925, 3595);	% Ca label only
 % Add intercept
 Xval = [ones(size(Xval, 1), 1) Xval];
 %==============================================================================
 							% Learning curve %
 %==============================================================================
-lambda = 0;
-[error_train, error_val] = learningCurve(X, y, Xval, yval, lambda);
+% lambda = 0;
+% [error_train, error_val] = learningCurve(X, y, Xval, yval, lambda);
 
-plot(1:m, error_train, 1:m, error_val);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
+% plot(1:m, error_train, 1:m, error_val);
+% title('Learning curve for linear regression')
+% legend('Train', 'Cross Validation')
+% xlabel('Number of training examples')
+% ylabel('Error')
 
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
+% fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+% for i = 1:m
+%     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+% end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 %==============================================================================
 			% Validation curve to choose the best lambda value
 %==============================================================================
@@ -44,8 +44,7 @@ ylabel('Error');
 
 fprintf('lambda\t\tTrain Error\tValidation Error\n');
 for i = 1:length(lambda_vec)
-	fprintf(' %f\t%f\t%f\n', ...
-            lambda_vec(i), error_train(i), error_val(i));
+	fprintf(' %f\t%f\t%f\n', lambda_vec(i), error_train(i), error_val(i));
 end
 
 fprintf('Program paused. Press enter to continue.\n');
