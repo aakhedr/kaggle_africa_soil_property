1234e5c929e27f79e42c09df8a6b80bcbc49afd9
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-02 17:43:43 +0200

    Fix a bug in learningCurve training subsets

--- learningCurve.m
+++ learningCurve.m
@@ -2,9 +2,9 @@ function [error_train, error_val] = learningCurve(X, y, Xval, yval, lambda)
 	m = size(X, 1); 			% number of training examples
 
 	for i = 1:m
-		theta = trainLinearReg(X, y, lambda);
+		theta = trainLinearReg(X(1:i, :), y(1:i, :), lambda);
 
-		error_train(i) = linRegCostFunc(X, y, theta, 0);
+		error_train(i) = linRegCostFunc(X(1:i, :), y(1:i, :), theta, 0);
 		error_val(i) = linRegCostFunc(Xval, yval, theta, 0);
 	end
 end
