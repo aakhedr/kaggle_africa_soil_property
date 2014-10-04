18351cf797b78ef743411ceac3e01a9eb7bb68aa
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-04 08:45:59 +0200

    Create master1 file and remove all the rest

--- P.m
+++ P.m
@@ -4,7 +4,7 @@ data = csvread('training.csv', 1, 1);
 % ==============================================================================
 % 60% of 1157 observations ~= 695 examples (training set)
 % ==============================================================================
-X = data(1:695, 1:3594); y = data(1:695, 3598);		% SOC label only
+X = data(1:695, 1:3594); y = data(1:695, 3596);		% P label only
 
 [m, n] = size(X);
 % add intercept term
@@ -13,7 +13,7 @@ X = [ones(m, 1) X];
 % ==============================================================================
 % 20% of 1157 observations ~= 231 examples (Cross validation set)
 % ==============================================================================
-Xval = data(696:926, 1:3594); yval = data(696:926, 3598);	% SOC label only
+Xval = data(696:926, 1:3594); yval = data(696:926, 3596);	% P label only
 % Add intercept
 Xval = [ones(size(Xval, 1), 1) Xval];
 
@@ -22,7 +22,7 @@ Xval = [ones(size(Xval, 1), 1) Xval];
 % ==============================================================================
 input_layer_size  = 3594;			% Number of features
 hidden_layer_size = 5000;			% The more the better
-num_labels = 1;						% SOC prediction
+num_labels = 1;						% P prediction
 
 initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
 initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
@@ -46,7 +46,7 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 % ==============================================================================
 % 20% of 1157 observations ~= 231 examples (Test set)
 % ==============================================================================
-Xtest = data(927:end, 1:3594); ytest = data(927:end, 3598);	% SOC label only
+Xtest = data(927:end, 1:3594); ytest = data(927:end, 3596);	% P label only
 % Add intercept
 Xtest = [ones(size(Xtest, 1), 1) Xtest];
 
@@ -60,4 +60,5 @@ SSE = sum((h2 - ytest).^2);
 fprintf('Sum of squared erros on the test set (231 examples) is: %f\n', SSE);
 
 % ==============================================================================
-
+% ==============================================================================
+% ==============================================================================
---
+++ master1.m
@@ -0,0 +1,98 @@
+clear; close all; clc;
+
+data = csvread('training.csv', 1, 1);
+
+input_layer_size  = 3594;			% Number of features
+hidden_layer_size = 5000;			% The more the better
+num_labels = 1;						% Ca prediction
+
+initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+
+X = data(:, 1:3594); [m, n] = size(X); X = [ones(m, 1) X]; lambda = 0.1;
+
+testData = csvread('sorted_test.csv', 1, 1); [k, l] = size(testData);
+Xtest = [ones(k, 1) testData];
+
+for i = 1:5
+	if i == 1
+		y = data(:, 3595);		% Ca label only
+
+		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
+			num_labels, X, y, lambda);
+
+		Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+		                 hidden_layer_size, (input_layer_size + 1));
+
+		Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+		                 num_labels, (hidden_layer_size + 1));
+
+		h1 = Xtest * Theta1';
+		Ca = [ones(size(h1, 1), 1) h1] * Theta2';
+
+	elseif i == 2
+		y = data(:, 3596);		% P label only
+
+		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
+			num_labels, X, y, lambda);
+
+		Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+		                 hidden_layer_size, (input_layer_size + 1));
+
+		Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+		                 num_labels, (hidden_layer_size + 1));
+
+		h1 = Xtest * Theta1';
+		P = [ones(size(h1, 1), 1) h1] * Theta2';
+
+	elseif i == 3
+		y = data(:, 3597);		% pH label only
+
+		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
+			num_labels, X, y, lambda);
+
+		Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+		                 hidden_layer_size, (input_layer_size + 1));
+
+		Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+		                 num_labels, (hidden_layer_size + 1));
+
+		h1 = Xtest * Theta1';
+		pH = [ones(size(h1, 1), 1) h1] * Theta2';
+
+	elseif i == 4
+		y = data(:, 3598);		% SOC label only
+
+		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
+			num_labels, X, y, lambda);
+
+		Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+		                 hidden_layer_size, (input_layer_size + 1));
+
+		Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+		                 num_labels, (hidden_layer_size + 1));
+
+		h1 = Xtest * Theta1';
+		SOC = [ones(size(h1, 1), 1) h1] * Theta2';
+
+	elseif i == 5
+		y = data(:, 3599);		% SAND label only
+
+		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
+			num_labels, X, y, lambda);
+
+		Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+		                 hidden_layer_size, (input_layer_size + 1));
+
+		Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+		                 num_labels, (hidden_layer_size + 1));
+
+		h1 = Xtest * Theta1';
+		SAND = [ones(size(h1, 1), 1) h1] * Theta2';
+	end
+end
+
+csvwrite('submission2.1.csv', [Ca P pH SOC SAND]);
+
