289a64ba84581b676e233b584f50a73d0a447042
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-06 23:13:31 +0200

    Tried several submissions

--- benchmark.py
+++ benchmark.py
@@ -11,16 +11,12 @@ train = pd.read_csv('training.csv')
 test = pd.read_csv('sorted_test.csv')
 labels = train[['Ca','P','pH','SOC','Sand']].values
 
-# train.drop(['Ca', 'P', 'pH', 'SOC', 'Sand', 'PIDN'], axis=1, inplace=True)
-# test.drop('PIDN', axis=1, inplace=True)
-
-train.drop(['PIDN'], axis=1, inplace=True)
-test.drop(['PIDN'], axis=1, inplace=True)
+train.drop(['Ca', 'P', 'pH', 'SOC', 'Sand', 'PIDN'], axis=1, inplace=True)
+test.drop('PIDN', axis=1, inplace=True)
 
 xtrain, xtest = np.array(train)[:,:3578], np.array(test)[:,:3578]
 
-
-sup_vec = svm.SVR(C=10000.0, verbose = 2)
+sup_vec = svm.SVR(C=500.0, verbose = 2)
 
 preds = np.zeros((xtest.shape[0], 5))
 for i in range(5):
@@ -34,5 +30,5 @@ sample['pH'] = preds[:,2]
 sample['SOC'] = preds[:,3]
 sample['Sand'] = preds[:,4]
 
-sample.to_csv('submissoin2.2.csv', index = False)
+sample.to_csv('submission2.5.csv', index = False)
 
