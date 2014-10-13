07911a3284f41a1e4cc09ab05848d1f0e0b1f4b4
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-13 16:20:09 +0200

    Try different hidden layer sizes with different lambdas

--- learningCurve_NN.m
+++ learningCurve_NN.m
@@ -1,13 +1,14 @@
-function [error_train, error_val] = learningCurve(X, y, Xval, yval, lambda, ...
+function [error_train, error_val] = learningCurve_NN(X, y, Xval, yval, lambda, ...
 	initial_nn_params, input_layer_size, hidden_layer_size, num_labels)
 
 	m = size(X, 1); 			% number of training examples
+    error_train = zeros(m, 1); error_val = zeros(size(Xval, 1), 1);
 
 	for i = 1:m
 		nn_params = trainNN(X(1:i, :), y(1:i, :), initial_nn_params, ... 
 			input_layer_size, hidden_layer_size, num_labels, lambda);
 
-		error_train(i) = nnCostFunction(X(1:i, :), y(1:i, :), theta, 0);
-		error_val(i) = nnCostFunction(Xval, yval, theta, 0);
+		error_train(i) = nnCostFunction(X(1:i, :), y(1:i, :), nn_params, 0);
+		error_val(i) = nnCostFunction(Xval, yval, nn_params, 0);
 	end
 end
--- try_diff_NNs.m
+++ try_diff_NNs.m
@@ -3,24 +3,22 @@ clear; close all; clc;
 data = csvread('training.csv', 1, 1);
 
 input_layer_size  = 3593;			% Number of features
-hidden_layer_size = 250;			% The more the better
-num_labels = 1;						% Ca prediction
+hidden_layer_size = 150;
+num_labels = 1;						% one of prediction values
 
 initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
 initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
 
 initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
-X = data(1:695, 1:3593); m = size(X, 1); X = [ones(m, 1) X]; lambda = 0.03;
+X = data(1:695, 1:3593); m = size(X, 1); X = [ones(m, 1) X]; lambda = .001;
 
 Xval = data(696:926, 1:3593);   Xval = [ones(size(Xval, 1), 1) Xval];
-Xtest = data(927:end, 1:3593);  Xtest = [ones(size(Xval, 1), 1) Xtest];
+Xtest = data(927:end, 1:3593);  Xtest = [ones(size(Xtest, 1), 1) Xtest];
 
 for i = 1:5
 	if i == 1
 		y = data(1:695, 3595);		% Ca label only
-        yval = data(696:926, 3595);
-        ytest = data(927:end, 3595);
 
 		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
 			num_labels, X, y, lambda);
@@ -36,8 +34,6 @@ for i = 1:5
 
 	elseif i == 2
 		y = data(1:695, 3596);		% P label only
-        yval = data(696:926, 3596);
-        ytest = data(927:end, 3596);
 
 		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
 			num_labels, X, y, lambda);
@@ -53,8 +49,6 @@ for i = 1:5
 
 	elseif i == 3
 		y = data(1:695, 3597);		% pH label only
-        yval = data(696:926, 3597);
-        ytest = data(927:end, 3597);
 
 		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
 			num_labels, X, y, lambda);
@@ -70,8 +64,6 @@ for i = 1:5
 
 	elseif i == 4
 		y = data(1:695, 3598);		% SOC label only
-        yval = data(696:926, 3598);
-        ytest = data(927:end, 3598);
 
 		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
 			num_labels, X, y, lambda);
@@ -87,8 +79,6 @@ for i = 1:5
 
 	elseif i == 5
 		y = data(1:695, 3599);		% SAND label only
-        yval = data(696:926, 3599);
-        ytest = data(927:end, 3599);
 
 		nn_params = trainNN(initial_nn_params, input_layer_size, hidden_layer_size, ...
 			num_labels, X, y, lambda);
@@ -103,11 +93,12 @@ for i = 1:5
 		SAND = [ones(size(h1, 1), 1) h1] * Theta2';
 	end
 end
-Ca_SSE = sum((Ca - yval).^2);
-P_SSE = sum((P - yval).^2);
-pH_SSE = sum((pH - yval).^2);
-SOC_SSE = sum((SOC - yval).^2);
-SAND_SSE = sum((SAND - yval).^2);
+
+Ca_SSE = sum((Ca - data(696:926, 3595)).^2);
+P_SSE = sum((P - data(696:926, 3596)).^2);
+pH_SSE = sum((pH - data(696:926, 3597)).^2);
+SOC_SSE = sum((SOC - data(696:926, 3598)).^2);
+SAND_SSE = sum((SAND - data(696:926, 3599)).^2);
 
 fprintf('Ca squared error: %f\n', Ca_SSE);
 fprintf('P squared error: %f\n', P_SSE);
