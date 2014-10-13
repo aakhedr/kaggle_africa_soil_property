275adb0450aa5f7c6455eda0cc9c019aad1e0c65
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-13 18:17:20 +0200

    Add 1 hidden layer to the Neural Network

--- try_diff_NNs.m
+++ try_diff_NNs.m
@@ -7,9 +7,10 @@ hidden_layer_size = 150;
 num_labels = 1;						% one of prediction values
 
 initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
-initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+initial_Theta2 = randInitializeWeights(hidden_layer_size, hidden_layer_size);
+initial_Theta3 = randInitializeWeights(hidden_layer_size, num_labels);
 
-initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:); initial_Theta3(:)];
 
 X = data(1:695, 1:3593); m = size(X, 1); X = [ones(m, 1) X]; lambda = .001;
 
